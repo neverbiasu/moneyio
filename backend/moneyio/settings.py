74b8111a50102a378326b67e2599c89d92d9d59a
neverbiasu <1751162157@qq.com>
2026-03-16 14:20:34 +0000

    fix: fallback when coolify secret key is empty

--- backend/moneyio/settings.py
+++ backend/moneyio/settings.py
@@ -21,9 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-sf9wpbkl4kx1i7n1m57mo6t)&)0yxr0quv&m3v1^)l!&nqnm7&",
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY") or (
+    "django-insecure-sf9wpbkl4kx1i7n1m57mo6t)&)0yxr0quv&m3v1^)l!&nqnm7&"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
